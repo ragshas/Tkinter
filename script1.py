98493bcef7e59e2bc014a5db72d390e18577ba7d
Raghab <ragshas@gmail.com>
2018-12-14 14:00:25 +0545

    adding of label, more text and completing it

--- script1.py
+++ script1.py
@@ -2,18 +2,34 @@ from tkinter import *
 
 window=Tk()
 
-def km_to_miles():
-    miles = float(e1_value.get())*1.6
-    t1.insert(END,miles)
+def kg_to_others():
+    gm = float(e1_value.get())*1000
+    pnd = float(e1_value.get())*2.20462
+    ounc = float(e1_value.get())*35.274
+    t1_gm.insert(END,gm)
+    t1_pnd.insert(END, pnd)
+    t1_oun.insert(END,ounc)
 
-b1=Button(window, text="Execute", command=km_to_miles)
-b1.grid(row=0,column=0)
+b1=Button(window, text="Convert", command=kg_to_others)
+b1.grid(row=0,column=3)
 
 e1_value=StringVar()
 e1=Entry(window, textvariable=e1_value)
-e1.grid(row=0,column=1)
+e1.grid(row=0,column=2)
 
-t1=Text(window, height=1, width=20)
-t1.grid(row=0,column=2)
+label = Label(window, text = "Kg")
+label.grid(row=0,column=1)
+
+
+t1_gm=Text(window, height=1, width=20)
+t1_gm.grid(row=1,column=1)
+
+t1_pnd = Text(window, height=1, width=20)
+t1_pnd.grid(row=1,column=2)
+
+t1_oun = Text(window, height=1, width=20)
+t1_oun.grid(row=1, column=3)
+
+
 
 window.mainloop()
